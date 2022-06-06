42c8f03fb2a7366cd4b103ffb8f6f839b4d21cda
Josh Nice <joshliamnice@gmail.com>
2022-06-06 09:11:28 +0100

    Added menu 'X'

--- js/menu.js
+++ js/menu.js
@@ -3,22 +3,28 @@ var currentPage = "home";
 
 var pageElements;
 
+var openMenuIcon, closeMenuIcon;
+
 window.onload = () => {
     pageElements = {
         menu: document.getElementById("menu"),
         home: document.getElementById("home"),
     }
+    openMenuIcon = document.getElementById("open-menu-icon");
+    closeMenuIcon = document.getElementById("close-menu-icon");
 }
 
 function menuClicked() {
     if (menuOpen) {
         pageElements[currentPage].style.display = "flex";
         pageElements.menu.style.display = "none";
-
+        openMenuIcon.style.display = "block";
+        closeMenuIcon.style.display = "none";
     } else {
         pageElements[currentPage].style.display = "none";
         pageElements.menu.style.display = "flex";
-
+        openMenuIcon.style.display = "none";
+        closeMenuIcon.style.display = "block";
     }
     menuOpen = !menuOpen;
 }
