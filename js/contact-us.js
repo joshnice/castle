2040371ec76ca03c4f6858f547ac397b6f284633
Josh Nice <joshliamnice@gmail.com>
2022-08-28 10:49:44 +0100

    Made validation easier to spot in UI

--- js/contact-us.js
+++ js/contact-us.js
@@ -1,39 +1,86 @@
+const INVALID_FORM_CLASS = "text-input-invalid";
+
 const formValues = {
     name: { value: null, touched: false, validation: () => formValues.name.valid = formValues.name.value.length > 2, valid: false },
     email: { value: null, touched: false, validation: () => formValues.email.valid = emailIsValid(formValues.email.value), valid: false },
-    subject: { value: null, touched: false, validation: () => formValues.subject.valid = formValues.name.value.length > 2, valid: false },
-    message: { value: null, touched: false, validation: () => formValues.message.valid = formValues.name.value.length > 2, valid: false },
+    subject: { value: null, touched: false, validation: () => formValues.subject.valid = formValues.subject.value.length > 2, valid: false },
+    message: { value: null, touched: false, validation: () => formValues.message.valid = formValues.message.value.length > 2, valid: false },
 }
 
 const submitButton = document.getElementById("submit-enquiry-form");
 submitButton.disabled = true;
-console.log("submitButton", submitButton);
 
-document.getElementById("name-input").addEventListener("input", (e) => {
+const nameInput = document.getElementById("name-input");
+const emailInput = document.getElementById("email-input");
+const subjectInput = document.getElementById("subject-input");
+const messageInput = document.getElementById("message-input");
+
+nameInput.addEventListener("input", (e) => {
     formValueChange(e.target.value, "name");
 });
 
-document.getElementById("email-input").addEventListener("input", (e) => {
+emailInput.addEventListener("input", (e) => {
     formValueChange(e.target.value, "email");
 });
 
-document.getElementById("subject-input").addEventListener("input", (e) => {
+subjectInput.addEventListener("input", (e) => {
     formValueChange(e.target.value, "subject");
 });
 
-document.getElementById("message-input").addEventListener("input", (e) => {
+messageInput.addEventListener("input", (e) => {
     formValueChange(e.target.value, "message");
 });
 
 function emailIsValid (email) {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
-}    
+}
 
-function formValueChange(value, accessor) {
+function invalidInput(input, valid) {
+    console.log("invalidInput", input, valid);
+    switch (input) {
+        case "name":
+            if (valid) {
+                nameInput.classList.remove(INVALID_FORM_CLASS);
+            } else {
+                nameInput.classList.add(INVALID_FORM_CLASS);
+            }
+            break;
+        case "email":
+            if (valid) {
+                emailInput.classList.remove(INVALID_FORM_CLASS);
+            } else {
+                emailInput.classList.add(INVALID_FORM_CLASS);
+            }
+            break;
+        case "subject":
+            if (valid) {
+                subjectInput.classList.remove(INVALID_FORM_CLASS);
+            } else {
+                subjectInput.classList.add(INVALID_FORM_CLASS);
+            }
+            break;
+        case "message":
+            if (valid) {
+                messageInput.classList.remove(INVALID_FORM_CLASS);
+            } else {
+                messageInput.classList.add(INVALID_FORM_CLASS);
+            }
+            break;
+        default: 
+            console.warn(`Could not found input ${input}`);
+    }
+}
+
+function changeFormValues(value, accessor) {
     formValues[accessor].value = value;
     formValues[accessor].valid = true;
     formValues[accessor].validation();
+}
+
+function formValueChange(value, accessor) {
+    changeFormValues(value, accessor);
     disableSubmitButton();
+    invalidInput(accessor, formValues[accessor].valid);
 }
 
 function disableSubmitButton() {
