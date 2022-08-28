c71bf6561223a6383eb662b3fb93ba6062afd89c
Josh Nice <joshliamnice@gmail.com>
2022-08-28 10:30:20 +0100

    Disable button if form is not valid

--- js/contact-us.js
+++ js/contact-us.js
@@ -1,16 +1,15 @@
-var formValues = {
+const formValues = {
     name: { value: null, touched: false, validation: () => formValues.name.valid = formValues.name.value.length > 2, valid: false },
     email: { value: null, touched: false, validation: () => formValues.email.valid = emailIsValid(formValues.email.value), valid: false },
     subject: { value: null, touched: false, validation: () => formValues.subject.valid = formValues.name.value.length > 2, valid: false },
     message: { value: null, touched: false, validation: () => formValues.message.valid = formValues.name.value.length > 2, valid: false },
 }
 
-function emailIsValid (email) {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
-}    
+const submitButton = document.getElementById("submit-enquiry-form");
+submitButton.disabled = true;
+console.log("submitButton", submitButton);
 
 document.getElementById("name-input").addEventListener("input", (e) => {
-    console.log("val");
     formValueChange(e.target.value, "name");
 });
 
@@ -26,15 +25,22 @@ document.getElementById("message-input").addEventListener("input", (e) => {
     formValueChange(e.target.value, "message");
 });
 
+function emailIsValid (email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+}    
+
 function formValueChange(value, accessor) {
     formValues[accessor].value = value;
     formValues[accessor].valid = true;
     formValues[accessor].validation();
+    disableSubmitButton();
 }
 
 function disableSubmitButton() {
     const validValues = Object.values(formValues).map(({ valid }) => valid);
-    return validValues.every((val) => val);
+    const valid = validValues.every((val) => val);
+    submitButton.disabled = !valid;
+
 }
 
 function submitForm() {
