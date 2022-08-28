83174902045b295b1eadbb4c05c28676c8be24f5
Josh Nice <joshliamnice@gmail.com>
2022-08-28 10:19:59 +0100

    Added form validation functions

--- js/contact-us.js
+++ js/contact-us.js
@@ -1,25 +1,40 @@
 var formValues = {
-    name: null,
-    email: null,
-    subject: null,
-    message: null,
+    name: { value: null, touched: false, validation: () => formValues.name.valid = formValues.name.value.length > 2, valid: false },
+    email: { value: null, touched: false, validation: () => formValues.email.valid = emailIsValid(formValues.email.value), valid: false },
+    subject: { value: null, touched: false, validation: () => formValues.subject.valid = formValues.name.value.length > 2, valid: false },
+    message: { value: null, touched: false, validation: () => formValues.message.valid = formValues.name.value.length > 2, valid: false },
 }
 
+function emailIsValid (email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+}    
+
 document.getElementById("name-input").addEventListener("change", (e) => {
-    formValues.name = e.target.value;
+    formValueChange(e.target.value, "name");
 });
 
 document.getElementById("email-input").addEventListener("change", (e) => {
-    formValues.email = e.target.value;
+    formValueChange(e.target.value, "email");
 });
 
 document.getElementById("subject-input").addEventListener("change", (e) => {
-    formValues.subject = e.target.value;
+    formValueChange(e.target.value, "subject");
 });
 
 document.getElementById("message-input").addEventListener("change", (e) => {
-    formValues.message = e.target.value;
+    formValueChange(e.target.value, "message");
 });
+
+function formValueChange(value, accessor) {
+    formValues[accessor].value = value;
+    formValues[accessor].valid = true;
+    formValues[accessor].validation();
+}
+
+function disableSubmitButton() {
+    const validValues = Object.values(formValues).map(({ valid }) => valid);
+    return validValues.every((val) => val);
+}
 
 function submitForm() {
     emailjs.init("VD8B22KlOjrjAzUNW");
